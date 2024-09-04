de8b38e79e7060a384fce79f946554831423f919
hiaayush30 <hiaayush30@gmail.com>
2024-09-04 12:57:07 +0530

    modules

---
+++ Modules/src/shapes.ts
@@ -0,0 +1,10 @@
+// import { Circle } from "./Circle";
+// import { Square } from "./Square";
+
+// //Re exporting
+// export {Circle,Square}
+
+//or
+
+export {Circle} from './Circle'
+export {Square} from './Square'
---
+++ Modules/src/storage.ts
@@ -0,0 +1,11 @@
+export default class Store{
+
+}
+
+class Compressor{}
+class Encryptor{}
+
+export enum Format{
+    Raw,
+    Compressed
+}
