042b5b41d04bc4232d64bb0aa597e0e353bc1629
aayush <hiaayush30@gmail.com>
2025-01-22 12:42:39 +0530

    Type Gaurds

--- 19.TypeGaurding.ts
+++ 19.TypeGaurding.ts
@@ -8,3 +8,26 @@ function abcd(arg: string | number) {
         arg.toLowerCase();
     }
 }
+
+class TVRemote{
+    switchOffTV(){
+        console.log('switching off TV')
+    } 
+}
+
+class CarRemote{
+    switchCarOff(){
+        console.log('switching off Car')
+    } 
+}
+
+const tv=new TVRemote();
+const car=new CarRemote();
+
+function switchOffKaro(device:TVRemote|CarRemote){
+    if(device instanceof TVRemote){
+        device.switchOffTV();
+    }else{
+        device.switchCarOff();
+    }
+}
